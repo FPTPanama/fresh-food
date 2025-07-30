795ae57b825a824debd16c6ce32a55d8fe4e0e23
Daniel Vazquez <daniel.vazquez@ascendmakers.com>
2025-07-30 20:34:02 +0200

    logistic add

--- app/[locale]/logistic/page.js
+++ app/[locale]/logistic/page.js
@@ -4,6 +4,7 @@ import ScrollButton from 'components/scroll-button/scroll-down';
 import { PiFarmFill } from 'react-icons/pi';
 import { FaTruckField, FaBoxesPacking, FaShip, FaTruckArrowRight } from 'react-icons/fa6';
 import { MdHouseboat } from 'react-icons/md';
+import { TbPointFilled } from 'react-icons/tb';
 import { PiShippingContainerFill } from 'react-icons/pi';
 import { FaTruckLoading } from 'react-icons/fa';
 import { FaAngleRight } from 'react-icons/fa';
@@ -19,56 +20,56 @@ const Logistic = async ({ params }) => {
       title: dictionary.logistic.ruta_tropical[0].TITLE,
       description: dictionary.logistic.ruta_tropical[0].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[0].specifications,
-      icon: <PiFarmFill size={160} />,
+      icon: <PiFarmFill size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[1].TITLE,
       description: dictionary.logistic.ruta_tropical[1].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[1].specifications,
-      icon: <FaTruckField size={160} />,
+      icon: <FaTruckField size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[2].TITLE,
       description: dictionary.logistic.ruta_tropical[2].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[2].specifications,
-      icon: <FaBoxesPacking size={160} />,
+      icon: <FaBoxesPacking size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[3].TITLE,
       description: dictionary.logistic.ruta_tropical[3].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[3].specifications,
-      icon: <FaTruckLoading size={160} />,
+      icon: <FaTruckLoading size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[4].TITLE,
       description: dictionary.logistic.ruta_tropical[4].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[4].specifications,
-      icon: <PiShippingContainerFill size={160} />,
+      icon: <PiShippingContainerFill size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[5].TITLE,
       description: dictionary.logistic.ruta_tropical[5].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[5].specifications,
-      icon: <FaShip size={160} />,
+      icon: <FaShip size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[6].TITLE,
       description: dictionary.logistic.ruta_tropical[6].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[6].specifications,
-      icon: <MdHouseboat size={160} />,
+      icon: <MdHouseboat size={50} />,
     },
     {
       title: dictionary.logistic.ruta_tropical[7].TITLE,
       description: dictionary.logistic.ruta_tropical[7].DESCRIPTION,
       especifications: dictionary.logistic.ruta_tropical[7].specifications,
-      icon: <FaTruckArrowRight size={160} />,
+      icon: <FaTruckArrowRight size={50} />,
     },
   ];
   return (
     <div className="responsiveWidth gap-10 text-greendark">
       <GeneralLayout dictionary={dictionary}>
         <section style={{ height: 'calc(100vh - 180px)' }} className="flex flex-col w-full items-center justify-center gap-7">
-          <ScrollButton dictionary={dictionary} />
+          <ScrollButton dictionary={dictionary} isFixed />
           <div className="flex items-center justify-center w-full">
             <div className="flex flex-col gap-3 w-1/3">
               <h1 className="text-l-800">{dictionary.logistic.Y_ESTA_ES_NUESTRA_RUTA}</h1>
@@ -89,25 +90,33 @@ const Logistic = async ({ params }) => {
             </div>
           </div>
         </section>
-        <section className="flex flex-col w-full gap-10">
+        <section className="flex flex-col w-full gap-20">
           {ruta_tropical.map((step, key) => {
             return (
-              <div key={key} className="grid grid-cols-3 w-full border-2 border-greendark p-10 rounded-2xl">
-                <div className="flex flex-col items-start justify-center w-full">
-                  <p className="font-black text-l-500">{key + 1}.</p>
-                  <p className="text-l-400 max-w-[350px] font-black">{step.title}</p>
-                  <p className="text-l-200 max-w-[350px]">{step.description}</p>
+              <div
+                style={{ height: 'calc(100vh - 180px)' }}
+                key={key}
+                className="flex flex-col w-full items-center justify-center gap-10 border-greenlight border-b"
+              >
+                <div className="flex items-center justify-start gap-2 w-full">
+                  <p className="font-black text-l-400">{key + 1}</p>
+                  <TbPointFilled />
+                  <p className="text-l-400 font-black">{step.title.toUpperCase()}</p>
                 </div>
-                <div className="flex items-center justify-center w-full">{step.icon}</div>
-                <div className="flex flex-col items-start justify-center w-full gap-5 list-outside">
-                  {step.especifications.map((spec, key) => {
-                    return (
-                      <div key={key} className="flex items-center w-full justify-start gap-4">
-                        <FaAngleRight className="w-1/6" size={20} />
-                        <p className="w-5/6">{spec}</p>
-                      </div>
-                    );
-                  })}
+                <div className="flex w-3/4 gap-10">
+                  <div className="flex flex-col items-start justify-center w-full">
+                    <p className="text-l-600 font-reg">{step.description}</p>
+                  </div>
+                  <div className="flex flex-col items-start justify-center w-full gap-5 list-outside">
+                    {step.especifications.map((spec, key) => {
+                      return (
+                        <div key={key} className="flex items-center w-full justify-start">
+                          <FaAngleRight className="w-1/12" size={20} />
+                          <p className="w-11/12">{spec}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
             );
