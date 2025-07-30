52655c2021c2a591e1d2b8d8bd5b180cddba7e47
Daniel Vazquez <daniel.vazquez@ascendmakers.com>
2025-07-30 23:24:34 +0200

    logistic fix

--- components/title-scroll-display/title-scroll-display.js
+++ components/title-scroll-display/title-scroll-display.js
@@ -8,11 +8,11 @@ const TitleScrollDisplay = ({ title, number }) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
+      whileInView={{ opacity: 1, y: 10 }}
       viewport={{ once: false, amount: 0.2 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="h-14 relative top-6 flex items-center justify-center border-greendark border-2 rounded-full gap-2 z-10 bg-fresh px-10 py-3">
+      <div className="h-12 relative top-4 flex items-center justify-center  rounded-full gap-2 z-10 bg-fresh px-10 py-3">
         <p className="font-black">{number}</p>
         <TbPointFilled />
         <p>{title}</p>
